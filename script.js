0acd44fe64bbf7b0307e36445ef6481dfe89b594
unknown <ja.frederico1@gmail.com>
2023-07-12 11:39:07 -0400

    .

--- script.js
+++ script.js
@@ -1,48 +1,94 @@
 var jogadores = []
 
+// Carregar jogadores do Local Storage, se disponível
+function carregarJogadores() {
+  var jogadoresSalvos = localStorage.getItem('jogadores')
+  if (jogadoresSalvos) {
+    jogadores = JSON.parse(jogadoresSalvos)
+    exibirJogadores()
+  }
+}
+
+// Adicionar jogador
 function adicionarJogador() {
   var jogadorInput = document.getElementById('player-input')
   var jogador = jogadorInput.value.trim()
 
   if (jogador !== '') {
-    jogadores.push(jogador)
-    jogadorInput.value = ''
-    exibirJogador(jogador, jogadores.length)
+    if (jogadores.length < 15) {
+      jogadores.push(jogador)
+      jogadorInput.value = ''
+      exibirJogadores()
+      salvarJogadores()
+    } else {
+      alert('O número máximo de jogadores é 15.')
+    }
   }
 }
 
-function exibirJogador(jogador, numJogador) {
+// Exibir jogadores
+function exibirJogadores() {
   var jogadoresList = document.getElementById('jogadores-list')
-  var jogadorItem = document.createElement('li')
-  jogadorItem.textContent = numJogador + '. ' + jogador
-  jogadoresList.appendChild(jogadorItem)
+  jogadoresList.innerHTML = ''
+
+  for (var i = 0; i < jogadores.length; i++) {
+    var jogadorItem = document.createElement('li')
+    jogadorItem.textContent = i + 1 + '. ' + jogadores[i]
+
+    var removerBotao = document.createElement('button')
+    removerBotao.innerHTML = ''
+    removerBotao.className = 'remover-button'
+    removerBotao.setAttribute('data-index', i) // Atribui o índice do jogador ao atributo data-index
+    removerBotao.addEventListener('click', function () {
+      var index = parseInt(this.getAttribute('data-index')) // Obtém o índice do jogador do atributo data-index
+      removerJogador(index)
+    })
+
+    jogadorItem.appendChild(removerBotao)
+
+    jogadoresList.appendChild(jogadorItem)
+  }
 }
 
-function exibirModal() {
-  var modal = document.getElementById('modal')
-  modal.style.display = 'block'
+// Remover jogador individual
+function removerJogador(index) {
+  jogadores.splice(index, 1)
+  exibirJogadores()
+  salvarJogadores()
 }
 
-function fecharModal() {
-  var modal = document.getElementById('modal')
-  modal.style.display = 'none'
+// Remover todos os jogadores e times
+function limparJogadoresETimes() {
+  jogadores = []
+  exibirJogadores()
+  var timesDiv = document.getElementById('times')
+  timesDiv.innerHTML = ''
+  salvarJogadores()
 }
 
+// Salvar jogadores no Local Storage
+function salvarJogadores() {
+  localStorage.setItem('jogadores', JSON.stringify(jogadores))
+}
+
+// Sortear times
 function sortearTimes() {
   if (jogadores.length === 0) {
     alert('Por favor, insira pelo menos um jogador antes de sortear os times.')
     return
   }
 
+  var jogadoresSorteados = jogadores.slice(0) // Faz uma cópia do array de jogadores
+
   // Embaralhar a lista de jogadores
-  jogadores.sort(function () {
+  jogadoresSorteados.sort(function () {
     return 0.5 - Math.random()
   })
 
   var timesDiv = document.getElementById('times')
   timesDiv.innerHTML = '' // Limpar os times anteriores
 
-  var numTimes = Math.ceil(jogadores.length / 5)
+  var numTimes = Math.min(3, Math.ceil(jogadoresSorteados.length / 5))
 
   for (var i = 0; i < numTimes; i++) {
     var time = document.createElement('div')
@@ -50,11 +96,11 @@ function sortearTimes() {
     time.innerHTML = '<h3>Time ' + (i + 1) + '</h3>'
 
     var startIndex = i * 5
-    var endIndex = Math.min(startIndex + 5, jogadores.length)
+    var endIndex = Math.min(startIndex + 5, jogadoresSorteados.length)
 
     for (var j = startIndex; j < endIndex; j++) {
       var jogador = document.createElement('p')
-      jogador.innerHTML = jogadores[j]
+      jogador.innerHTML = jogadoresSorteados[j]
       time.appendChild(jogador)
     }
 
@@ -62,11 +108,15 @@ function sortearTimes() {
   }
 }
 
-function limparTimes() {
-  jogadores = []
-  var jogadoresList = document.getElementById('jogadores-list')
-  jogadoresList.innerHTML = ''
+// Evento de carga da página
+window.addEventListener('load', function () {
+  carregarJogadores()
+})
 
-  var timesDiv = document.getElementById('times')
-  timesDiv.innerHTML = ''
+// Remover jogador individual
+function removerJogador(jogadorItem) {
+  var index = Array.from(jogadorItem.parentNode.children).indexOf(jogadorItem)
+  jogadores.splice(index, 1)
+  exibirJogadores()
+  salvarJogadores()
 }
